69a4f995883387391ef1c5bf7c199e7ca9e2a566
Pieter Lewyllie <plewyllie@gmail.com>
2016-12-01 12:56:47 +0100

    oops! numbers can be longer than 1 char :-)

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-inputd = "R5, L5, R5, R3"
+inputd = "L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4"
 
 moves = inputd.replace(" ", "").split(",")
 
@@ -8,36 +8,40 @@ horizontal = 0
 
 for move in moves:
 	print("move " + move)
+	direc = move[0]
+	nb = int(move[1:])
 	if facing == "N":
-		if move[0] == "R":
-			horizontal += int(move[1])
+		if direc == "R":
+			horizontal += nb
 			facing = "E"
-		if move[0] == "L":
-			horizontal -= int(move[1])
+		if direc == "L":
+			horizontal -= nb
 			facing = "W"
 	elif facing == "W":
-		if move[0] == "R":
-			vertical += int(move[1])
+		if direc == "R":
+			vertical += nb
 			facing = "N"
-		if move[0] == "L":
-			vertical -= int(move[1])
+		if direc == "L":
+			vertical -= nb
 			facing = "S"
 	elif facing == "E":
-		if move[0] == "R":
-			vertical -= int(move[1])
+		if direc == "R":
+			vertical -= nb
 			facing = "S"
-		if move[0] == "L":
-			vertical += int(move[1])
+		if direc == "L":
+			vertical += nb
 			facing = "N"
 	elif facing == "S":
-		if move[0] == "R":
-			horizontal -= int(move[1])
+		if direc == "R":
+			horizontal -= nb
 			facing = "W"
-		if move[0] == "L":
-			horizontal += int(move[1])
+		if direc == "L":
+			horizontal += nb
 			facing = "E"
 
 print("Vertical " + str(vertical) + " and horizontal " + str(horizontal))
+print("Total blocks away is " + str(vertical + horizontal))
+
 
 
 
