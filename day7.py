9136b780c30ae5c69fb088bde5367b1f8790f81b
Pieter Lewyllie <plewyllie@gmail.com>
2016-12-07 21:21:28 +0100

    using regex library which has "overlapped" parameter to get overlapping regex matches

--- day7.py
+++ day7.py
@@ -2,10 +2,11 @@
 """--- Day 7: Internet Protocol Version 7 ---"""
 
 FILE = 'input.txt'
-DEBUG_INPUT = "epmnxkubnsnykyubv[ydzhcoytayiqmxlv]edmbahbircojbkmrg[dlxyprugefqzkum]svdaxilkrkukfg[eacekyzjchfpzghltn]ofwgevwnnwhoivrevueaj\nabba[mnop]qrst\naabcd[bddb]xyyx\naaaa[qwer]tyui\nioxxoj[asdfgh]zxcvbn\nifoxxorj[asoxxodfgh]zxoxxocvbn\nvjqhodfzrrqjshbhx[lezezbbswydnjnz]ejcflwytgzvyigz[hjdilpgdyzfkloa]mxtkrysovvotkuyekba"
+DEBUG_INPUT = "zazbz[bzb]cdb"
+
 DEBUG = False
 
-import re
+import regex as re
 
 def init(file):
     with open(file) as f:
@@ -14,26 +15,26 @@ def init(file):
 
 def str_has_ABBA(s):
     res = re.search(r'(.)(?!\1)(.)\2\1', s)
-    if DEBUG:
-        print("matching on " + s)
     if res:
-        print("Matched " + res.group())
         return res.group()
     else:
         return False
 
 def str_has_ABA(s):
-    res = re.search(r'(.)(?!\1)(.)\1', s)
-    if DEBUG:
-        print("matching on " + s)
+    res = [m.group() for m in re.finditer(r'(.)(?!\1)(.)\1', s, overlapped=True)]
     if res:
-        print("Matched " + res.group())
-        return res.group()
+        print("Matched")
+        print(res)
+        return res
     else:
         return False
 
-def str_has_BAB_(s, bab):
-    pass
+def str_has_BAB(s, aba):
+    bab = aba[1] + aba[0] + aba[1]
+    if bab in s:
+        return True
+    else:
+        return False
 
 class IPv7:
 
@@ -43,6 +44,7 @@ class IPv7:
         iterator = re.finditer(r'\[.*?\]', ipv7str)
         start = 0
         self.tls = False
+        self.ssl = False
 
         supernets = []
         hypernets = []
@@ -55,18 +57,37 @@ class IPv7:
             hypernets.append(str2)
         supernets.append(ipv7str[start:])
 
+        print(supernets)
+        print(hypernets)
+
+        abas = []
         for supernet in supernets:
             if str_has_ABBA(supernet):
                 self.tls = True
+
+            aba = str_has_ABA(supernet)
+            if aba:
+                abas += aba
+
+        print("Testing for abas:")
+        print(abas)
+        for aba in abas:
+            for hypernet in hypernets:
+                if str_has_BAB(hypernet, aba):
+                    self.ssl = True
+
         for hypernet in hypernets:
             if str_has_ABBA(hypernet):
                 self.tls = False
 
-            #if aba and str_has_BAB(str2, aba):
-             #   self.ssl = True
+
+        #if aba and str_has_BAB(str2, aba):
+            #   self.ssl = True
 
     def tls_support(self):
         return self.tls
+    def ssl_support(self):
+        return self.ssl
         
 
 
@@ -79,11 +100,19 @@ if __name__ == '__main__':
     inputd_lines = inputd.splitlines()
 
     tls_support_nb = 0
+    ssl_support_nb = 0
     for line in inputd_lines:
         ipv7 = IPv7(line)
         if ipv7.tls_support() == True:
             print("IPv7 " + ipv7.ipv7str +  " supports TLS")
             tls_support_nb += 1
+        if ipv7.ssl_support() == True:
+            print("IPv7 " + ipv7.ipv7str +  " supports SSL")
+            ssl_support_nb += 1
+
+
     print("There are " + str(tls_support_nb) + " IPv7 supporting TLS")
+    print("There are " + str(ssl_support_nb) + " IPv7 supporting SSL")
+
 
 
